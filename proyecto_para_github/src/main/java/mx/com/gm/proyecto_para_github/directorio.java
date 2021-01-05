50279f0eb1187be160eceb35a20f111eddfb9b2d
Cruz <andressmaldonado1@gmail.com>
2021-01-05 16:46:53 -0400

    ejeplo de repositorio con netbeans

---
+++ proyecto_para_github/src/main/java/mx/com/gm/proyecto_para_github/directorio.java
@@ -0,0 +1,14 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package mx.com.gm.proyecto_para_github;
+
+/**
+ *
+ * @author USUARIO
+ */
+public class directorio {
+    
+}
---
+++ proyecto_para_github/src/main/java/mx/com/gm/proyecto_para_github/proyecto_para_github.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package mx.com.gm.proyecto_para_github;
+
+/**
+ *
+ * @author USUARIO
+ */
+public class proyecto_para_github {
+    public static void main(String[] args) {
+        System.out.println("hola");
+    }
+}
